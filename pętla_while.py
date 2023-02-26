a57fd43d190cb7107846f61350a55c96f36c5888
Klisiuu777 <126404805+Klisiuu777@users.noreply.github.com>
2023-02-26 18:17:23 +0100

    Add files via upload

---
+++ pętla_while.py
@@ -0,0 +1,27 @@
+#warunek wykorzystuje zmienną sterującą i sprawdzany jest na początku pętli.
+#aby pętla się skończyła zmienna sterująca musi zmienić się wewnątrz pętli.
+a = 10
+while a == 10:
+    print(a)
+print("koniec")
+
+#########################################################################################
+
+#nieskończona pętla.
+a = 10
+while a > 0:
+    a = a + 1
+    print(a)
+print("koniec")
+
+#########################################################################################
+
+#czekamy aż user poda dobrą odpowiedź.
+#gdy ją poda to po kliknięciu enter program się zakończy.
+odpowiedz = " "
+proby = 0
+while odpowiedz != 't' and odpowiedz != 'n':
+    proby = proby + 1
+    odpowiedz = input("koniec? (t/n)")
+print(f"Wybrales " {odpowiedz})
+print("Ilosc prob: " {proby})
