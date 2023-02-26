1600912e2d064f1576845410acc1b233e3c440ad
Klisiuu777 <126404805+Klisiuu777@users.noreply.github.com>
2023-02-26 18:23:07 +0100

    Create zamiany.py

---
+++ zamiany.py
@@ -0,0 +1,20 @@
+#zamiana wartości na napis -- str():
+
+print( str(a) + b) #łączenie napisów
+
+ 
+
+#zamiana napisu liczbowego na wartość -- int()
+# oraz  --  float()
+
+print( int("1234") + 12 ) #zamiana liczb całk
+
+print( float("4.23") * 6 ) #zamiana liczby zmiennoprzec
+
+ 
+
+#zamiana systemów liczbowych
+
+print( int("010111",2))
+
+print( int("ala",32 ))
